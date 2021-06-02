ff4e28a948fafb21f0d7443fdb8e9202a3b7f3c3
欧阳宇轩 <365261277@qq.com>
2021-06-02 15:53:46 +0800

    项目初始化

---
+++ common/service-util/src/main/java/com/atguigu/common/config/MybatisPlusConfig.java
@@ -0,0 +1,26 @@
+package com.atguigu.common.config;
+
+import com.baomidou.mybatisplus.extension.plugins.OptimisticLockerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@MapperScan("com.atguigu.*.mapper")
+public class MybatisPlusConfig {
+
+    //注册乐观锁插件
+    @Bean
+    public OptimisticLockerInterceptor optimisticLockerInterceptor(){
+        return new OptimisticLockerInterceptor();
+    }
+
+
+    //注册分页插件
+    @Bean
+    public PaginationInterceptor paginationInterceptor(){
+        return new PaginationInterceptor();
+    }
+
+}
